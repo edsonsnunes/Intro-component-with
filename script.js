b472ac73314e4aa46d8762f3e509719b8000cec7
edson_nunes <edsonnunes05@icloud.com>
2022-03-11 16:53:44 -0300

    inicio-js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function verificar(){
-    nome = document.getElementById('nome')
-}
+        var nome = document.getElementById('nome') 
+        var res = document.getElementById('res')
+        if(nome.value.length == 0){
+            res.innerHTML = `O nome não pode ficar vazio`
+            nome.classList.remove("normalinput")
+            nome.classList.add("invalidinput")
+        }
+       
+    }
