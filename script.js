4a1705b2790c1befac018bae167573493149ffbd
edson_nunes <edsonnunes05@icloud.com>
2022-03-14 08:43:25 -0300

    Alteração javaScript

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ function verificar(){
         var res = document.getElementById('res')
         if(nome.value.length == 0){
             res.innerHTML = `O nome não pode ficar vazio`
-            nome.classList.remove("normalinput")
-            nome.classList.add("invalidinput")
+            
+        } if(sobrenome.value.length == 0){
+            res_1.innerHTML = `O sobrenome não pode ficar vazio`
+        } if(email.value.length == 0){
+            res_2.innerHTML = `O email não pode ficar vazio`
+        } if(senha.value.length == 0){
+            res_3.innerHTML = `A senha não pode ficar vazia`
         }
-       
     }
